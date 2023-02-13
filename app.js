40e6e6192edff20aed70e75cc3feaac8fb6775b1
Adrian <adrianlica27@gmail.com>
2023-02-13 21:27:49 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let icons = document.querySelectorAll('.icon');
+let stats = document.getElementsByClassName('stats')[0];
+
+console.log(icons);
+
+// icons.forEach((element) => {
+//   element.addEventListener('click', () => {
+//     for(let i = 0; i < )
+//   });
+// });
